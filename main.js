b3c53bca2b3fe774ebcfc13a439bd468a8365f37
Dejan Stjepanovic <ds2q88@gmail.com>
2018-05-01 22:56:25 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+//  The function will increment a global variable by one each time it is invoked,
+let global = 1 
+// Your job is to write a function that accepts any band name as an argument.
+
+const bandName = function (bandName)
+
+//   and return that number, and the band name concatenated together.
+{ console.log(`${global}. ${bandName}`)
+global +=1
+}
+    
+    bandName('metallica')
+    bandName('godspeed you black emperor')
+    bandName('Muse')
